ee1246818d0b5b0d1b782bfb755b69b78687c6f7
jiangdingxing <1632914230@qq.com>
2020-07-12 23:58:09 +0800

    集群版Eureka

---
+++ cloud-eureka-server7002/src/main/java/org/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package org.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jdx
+ * @date 2020-07-12 23:31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
