83547ae5ba31be2f0fec7bd5a0e53d9760db0f04
jiangdingxing <1632914230@qq.com>
2020-08-10 17:59:30 +0800

    Zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author jdx
+ * @date 2020-07-31 15:06
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk(){
+        return "springcloud wiht zookeeper:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
